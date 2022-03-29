f1961de10282f68efde8bd589dbb16973072013a
Lívia Minucelli <liviawrk@gmail.com>
2022-03-28 21:24:54 -0300

    feat: add styles on nav and buttonDynamic.

---
+++ src/components/DynamicButton/Button.tsx
@@ -0,0 +1,43 @@
+import './Button.scss';
+
+//possíveis estilos do botão.
+const STYLES = [
+    "primary",
+    "nav",
+    "secondary",
+    "disabled",
+]
+
+interface ButtonProps {
+    children?: string;
+    type?: "button" | "submit" | "reset";
+    onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    buttonStyle?: string;
+    disabled?: boolean;
+    className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    children,
+    type,
+    onClick,
+    buttonStyle,
+    disabled,
+    className
+}) => {
+    //If a valid buttonStyle exists it will be applied, otherwise, by default the style [0] -> primary will be set.
+    const checkButtonStyle = (buttonStyle && STYLES.includes(buttonStyle)) ? buttonStyle : STYLES[0];
+
+    return (
+        <button
+            className={`ui-button ${checkButtonStyle} ${className}`}
+            onClick={onClick}
+            type={type}
+            disabled={disabled}>
+            {children}
+        </button>
+    );
+
+}
+
+export default Button;
