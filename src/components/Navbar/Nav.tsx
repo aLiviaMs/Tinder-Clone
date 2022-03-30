e5d0473e91814f04179739f66ef6cce1c327354c
Lívia Minucelli <liviawrk@gmail.com>
2022-03-29 23:46:12 -0300

    feat: add AuthModal UI + animations.

--- src/components/Navbar/Nav.tsx
+++ src/components/Navbar/Nav.tsx
@@ -7,14 +7,22 @@ interface INavProps {
     className?: string;
     minimal?: boolean;
     authToken: boolean;
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+    showModal: boolean;
+    setIsSignUp: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const Nav: React.FC<INavProps> = ({ className, minimal, authToken }) => {
+const Nav: React.FC<INavProps> = ({ className, minimal, authToken, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+        setShowModal(true);
+        setIsSignUp(false);
+    }
+
     return (
         <nav className={className}>
             <div className="logo-container"><img className='logo' src={minimal ? colorLogo : logo} alt="Tinder logo" /></div>
 
-            {!authToken && !minimal && <Button buttonStyle='nav'>Log in </Button>}
+            {!authToken && !minimal && <Button buttonStyle='nav' onClick={handleClick} disabled={showModal} className="btn-login">Log in </Button>}
         </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,20 +11,28 @@ import './Home.scss';
 
 const Home = () => {
     const [showModal, setShowModal] = useState<boolean>(false);
+    const [isSignUp, setIsSignUp] = useState<boolean>(true);
     const authToken: boolean = false;
 
     const handleClick = () => {
         setShowModal(true);
+        setIsSignUp(true)
     }
 
     return (
         <div className="overlay">
-            <Nav className="text-align-center" authToken={authToken} />
+            <Nav
+                className="text-align-center"
+                authToken={authToken}
+                setShowModal={setShowModal}
+                showModal={showModal}
+                setIsSignUp={setIsSignUp}
+            />
             <main id="home" className="text-align-center">
-                <h1>Swipe Right®</h1>
+                <h1 className="primary-title">Swipe Right®</h1>
                 <Button buttonStyle="primary" onClick={handleClick}>{authToken ? 'Signout' : 'Create Account'}</Button>
                 {showModal && (
-                    <AuthModal setShowModal={setShowModal} />
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp} />
                 )}
             </main>
         </div>
