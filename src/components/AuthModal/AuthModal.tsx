314c5e50b79918149276a0a740d79f9de6fc8a27
Lívia Minucelli <liviawrk@gmail.com>
2022-03-29 22:08:00 -0300

    feat: add close-btn style and animation on AuthModal.

---
+++ src/components/AuthModal/AuthModal.tsx
@@ -0,0 +1,19 @@
+import './AuthModal.scss';
+
+interface IAuthModalProps {
+    setShowModal: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AuthModal: React.FC<IAuthModalProps> = ({ setShowModal }) => {
+    const handleCloseClick = () => {
+        setShowModal(false);
+    }
+
+    return (
+        <div>
+            <button aria-hidden="false" aria-label="Close" className='close-btn' onClick={handleCloseClick}><svg viewBox="0 0 24 24" width="24px" height="24px" focusable="false" aria-hidden="true" role="presentation" className='close-icon'><path d="M14.926 12.56v-1.14l5.282 5.288c1.056.977 1.056 2.441 0 3.499-.813 1.057-2.438 1.057-3.413 0L11.512 15h1.138l-5.363 5.125c-.975 1.058-2.438 1.058-3.495 0-1.056-.813-1.056-2.44 0-3.417l5.201-5.288v1.14L3.873 7.27c-1.137-.976-1.137-2.44 0-3.417a1.973 1.973 0 0 1 3.251 0l5.282 5.207H11.27l5.444-5.207c.975-1.139 2.438-1.139 3.413 0 1.057.814 1.057 2.44 0 3.417l-5.2 5.288z"></path></svg></button>
+        </div>
+    )
+}
+
+export default AuthModal; 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
+/*React imports */
+import { useEffect, useState } from "react";
+
+/*Components imports */
 import Button from "../components/DynamicButton/Button";
 import Nav from "../components/Navbar/Nav";
+import AuthModal from "../components/AuthModal/AuthModal";
+
+/*Styles imports */
 import './Home.scss';
 
 const Home = () => {
+    const [showModal, setShowModal] = useState<boolean>(false);
     const authToken: boolean = false;
 
     const handleClick = () => {
-        console.log('clicked');
+        setShowModal(true);
     }
 
     return (
         <div className="overlay">
             <Nav className="text-align-center" authToken={authToken} />
             <main id="home" className="text-align-center">
-                <h1>Swipe Right</h1>
+                <h1>Swipe Right®</h1>
                 <Button buttonStyle="primary" onClick={handleClick}>{authToken ? 'Signout' : 'Create Account'}</Button>
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} />
+                )}
             </main>
         </div>
     )
